cfc05d46545b0fc4f46857ba1536a3c7520af742
Prince-Arora <19princearora95@gmail.com>
2017-12-31 16:40:51 +0530

    write database

--- app/src/main/java/com/example/laptop/firebasicsall/MainActivity.java
+++ app/src/main/java/com/example/laptop/firebasicsall/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.laptop.firebasicsall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button submit;
+    EditText edit;
+    private DatabaseReference mDatabaseref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabaseref=FirebaseDatabase.getInstance().getReference();
+        edit=(EditText)findViewById(R.id.Edit1);
+        submit=(Button)findViewById(R.id.Button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view)
+        {
+            String text=edit.getText().toString();
+            mDatabaseref.child("Name").setValue(text);
+            Toast.makeText(getApplicationContext(),"success",Toast.LENGTH_SHORT).show();
+        }
+        });
+
     }
 }
