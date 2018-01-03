28f1cf11d093819990f91d9a8aa5050f9437d690
Prince-Arora <19princearora95@gmail.com>
2018-01-03 10:38:50 +0530

    complex data

--- app/src/main/java/com/example/laptop/firebasicsall/MainActivity.java
+++ app/src/main/java/com/example/laptop/firebasicsall/MainActivity.java
@@ -7,15 +7,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button submit;
-    EditText edit;
+    EditText edit,edit1;
     private DatabaseReference mDatabaseref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mDatabaseref=FirebaseDatabase.getInstance().getReference();
         edit=(EditText)findViewById(R.id.Edit1);
+        edit1=(EditText)findViewById(R.id.Edit2);
         submit=(Button)findViewById(R.id.Button);
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -30,8 +35,26 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view)
         {
             String text=edit.getText().toString();
-            mDatabaseref.child("Name").setValue(text);
-            Toast.makeText(getApplicationContext(),"success",Toast.LENGTH_SHORT).show();
+            String Email=edit1.getText().toString();
+            HashMap<String,String> data=new HashMap<String, String>();
+            data.put("Name",text);
+            data.put("Email",Email);
+            mDatabaseref.push().setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+                public void onComplete(Task<Void> task)
+            {
+            if(task.isSuccessful())
+            {
+                Toast.makeText(getApplicationContext(), "suceeded", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+
+            }
+            }
+
+            });
         }
         });
 
